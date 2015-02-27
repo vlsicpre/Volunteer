ecdd8087a986f613e85567028359b18453b69aca
vlsicpre <vlsicpre@outlook.com>
2015-02-27 15:17:27 -0600

    js

--- CommunityYouthConcepts/wp-content/themes/cyconcepts/scripts/ActivitySearch.js
+++ CommunityYouthConcepts/wp-content/themes/cyconcepts/scripts/ActivitySearch.js
@@ -36,46 +36,43 @@ function InitializeSliders() {
     // On after slide change
     $('.center').on('afterChange', function (event, slick, currentSlide, nextSlide) {
         var issueid = $(slick.$slides.get(currentSlide)).attr("issueid");
+
+
+
+
     });
 
-    var interests = GetCategories("interest");
-    var issues = GetCategories("issue");
-    var intervals = GetCategories("time");
-    //LoadCriteriaSliderData("Interests", interests);
-   // LoadCriteriaSliderData("Issues", issues);
-   // LoadCriteriaSliderData("Intervals", intervals);    
+    var interests = LoadCategories("Interests");
+    var issues = LoadCategories("Issues");
+    var intervals = LoadCategories("Intervals"); 
 }
 
-function GetCategories(categoryId)
+function LoadCategories(categoryId)
 {
     var serviceURL = "http://www.cyconcepts.org/wp-json/taxonomies/" + "issues" + "/terms";
+    var isDEV = document.location.href.indexOf('localhost') > 0;
+    if (isDEV) {
+        serviceURL = "/TestData/issues.xml";
+    }
 
-
-    //$.ajax({
-    //    url: serviceURL,
-    //    dataType: 'json',
-    //    data: data,
-    //    success: function (data) { alert('test');}
-    //});
-
-    //$.get(serviceURL, function (data) {
-    //    alert(data);
-    //}, "json");
-
-
-    //$.ajax({
-    //    dataType: "json",
-    //    url: serviceURL,
-    //    data: null,
-    //    success: function (data) { alert(data);}
-    //});
+    var res = $.ajax({
+        url: serviceURL,
+        dataType: 'json',
+        success: function (data) {
+            LoadCriteriaSliderData(categoryId, data);
+        },
+        error: function (error) {
+            alert(error);
+        }
+    });
 }
 
 function LoadCriteriaSliderData(sliderId, listData)
 {
-    for (var i = 0; i < listData.Length; i++)
+    for (var i = 0; i < listData.length; i++)
     {
-        $(sliderId).slick('slickAdd', "<div issueid='"+  listData[i].Id +"'>"+ listData[i].Name+"</div>");
+        $("#" + sliderId).slick('slickAdd', "<div issueid='" + listData[i].ID + "'><img src='" + listData[i].image_url + "' />"
+             + listData[i].name +   "</div>");
     }
 }
 
