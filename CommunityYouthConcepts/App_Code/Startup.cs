2e389103d6d911d1b2c1a071d031c9b0ba00dc0b
vlsicpre <vlsicpre@outlook.com>
2015-02-27 08:59:26 -0600

    folder structure update

---
+++ CommunityYouthConcepts/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CommunityYouthConcepts.Startup))]
+namespace CommunityYouthConcepts
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ CommunityYouthConcepts/wp-content/themes/cyconcepts/scripts/ActivitySearch.js
@@ -0,0 +1,48 @@
+﻿
+$(document).ready(function () {
+    InitializeSliders();
+});
+
+
+function InitializeSliders() {
+    $('.center').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [
+          {
+              breakpoint: 768,
+              settings: {
+                  arrows: false,
+                  centerMode: true,
+                  centerPadding: '40px',
+                  slidesToShow: 3
+              }
+          },
+          {
+              breakpoint: 480,
+              settings: {
+                  arrows: false,
+                  centerMode: true,
+                  centerPadding: '40px',
+                  slidesToShow: 1
+              }
+          }
+        ]
+    });
+
+
+    // On before slide change
+    $('.center').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+        alert('before');
+        console.log(nextSlide);
+    });
+
+    // On before slide change
+    $('.center').on('afterChange', function (event, slick, currentSlide, nextSlide) {
+        alert('after');
+        console.log(nextSlide);
+    });
+
+}
+
