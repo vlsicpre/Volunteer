9ea3bddec7bc3a18c61c8ae05fbe556bd37980d5
vlsicpre <vlsicpre@outlook.com>
2015-03-07 22:41:37 -0600

    Recreate solution with proper structure. Add service proxy to make development against the live service possible. Updated js file to use service proxy.

---
+++ CommunityYouthConcepts/ServiceProxy/Controllers/PostController.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ServiceProxy.Controllers
+{
+    public class PostController : ApiController
+    {
+        public JToken GetPosts()
+        {
+
+            string url = "http://cyconcepts.org/wp-json/posts?type=volunteer_project" + Request.RequestUri.Query.TrimStart('?');
+            string response = "";
+            using (var webClient = new WebClient())
+            {
+                response = webClient.DownloadString(url);
+            }
+            return JArray.Parse(response);
+        }
+
+        public JToken GetCategories(string categoryId)
+        {
+            string url = String.Format("http://cyconcepts.org/wp-json/taxonomies/{0}/terms", categoryId);
+            string response = "";
+            using (var webClient = new WebClient())
+            {
+                response = webClient.DownloadString(url);                
+            }
+            return JArray.Parse(response);
+        }
+    }
+}
